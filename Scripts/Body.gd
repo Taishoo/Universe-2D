35e11e1dbd04ac066c716169ed328bae1a48fc19
Albert Jr <Kitamoriken1@gmail.com>
2022-06-02 19:32:49 +0800

    Added speed normalize equation

--- Scripts/Body.gd
+++ Scripts/Body.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 const TYPE: String = "BODY"
 const Math = preload("res://Scripts/FrameWorks/Math.gd")
-const Engine = preload("res://Scripts/FrameWorks/Engine.gd")
+const Enginex = preload("res://Scripts/FrameWorks/Engine.gd")
 
 onready var math:Object = Math.new()
-onready var engine:Object = Engine.new()
+onready var engine:Object = Enginex.new()
 onready var pointer = get_node("RayCast2D")
 
 export var body_name: String = "earth"
@@ -16,10 +16,11 @@ export var diameter:float = 1.2756 # x10^4 km
 export var temperature: float = 0.0 # K
 
 var bodies:Array = []
-var time: float = 0 # t = t_universe + Δ_game
+var time: float = 0 # t = t_universe
 var velocity:Vector2 = Vector2(0,0) * 0 # V * (pixel/t)
 var momentum: Vector2 # kg/m^2/s
 var speed: float
+var speed_norm: float
 
 func _ready() -> void:
 	# yield(get_tree().create_timer(0.2), "timeout")
@@ -30,11 +31,11 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
 	preset(delta)
 	transform()
-	kinetic()
+	kinetic(delta)
 
 
-func preset(delta) -> void:
-	time = Global.UNIVERSE_TIME + delta
+func preset(_delta: float) -> void:
+	time = Global.UNIVERSE_TIME
 	bodies = engine.get_all_bodies(self, get_parent())
 
 
@@ -43,24 +44,32 @@ func transform() -> void:
 	diameter = pow((volume * 3) / (4 * math.pi), (1.0/3.0)) * 2 # d = ( (v*3/4*pi)^(1/3) ) * 2
 	gravity = ((math.G * mass) / pow(diameter/2, 2)) * 0.1  # g = G*M/r^2
 	self.scale = engine.lerp_vector2(self.scale, Vector2(diameter,diameter), .3)
-	self.z_index = mass * density
+	self.z_index = int(mass * density)
 
 
-func kinetic() -> void:
-	# →L = →p * r
-	momentum = velocity * mass # →p = m * →v
+func kinetic(delta: float) -> void:
 	var relational_data = engine.get_all_relational_data(self, bodies) # F = G * m1 * m2 / r^2
 	var angular_vector_sum = math.sum_of_angle(relational_data) # F1v = Σ F2v + F3v + F4v...	
 	var force_net = math.pythagorean_theorem(angular_vector_sum.x, angular_vector_sum.y) # F1_net = √(F1x^2)+(F1y^2)
-	velocity = velocity + ((angular_vector_sum  * force_net) * time)
-	speed = math.pythagorean_theorem(velocity.x, velocity.y)
+	velocity += (angular_vector_sum  * force_net) * time
 	velocity = engine.lerp_vector2(velocity, Vector2.ZERO, 0)
+	momentum = velocity * mass # →p = m * →v
+
+	# my equation of normalizing speed in godot engine
+	var exponent = log(1/time) / log(10) # exp = log(1/t)
+	speed_norm = (speed * pow(3 + delta * 10, exponent)) # s_norm = s * (3 + Δt*10)^exp
+
+	var vx = velocity
+	speed = math.pythagorean_theorem(vx.x, vx.y)
+
+	if get_name() == "Planet":
+		print(speed_norm)
 
 	pointer.set_cast_to(velocity / diameter)
 	var _catch_move_and_slide = move_and_slide(velocity)
 
-func collide(retained: float, obj: KinematicBody2D) -> void:
-	var merge_speed: float = ((1 / speed)) * (1/time)
+func collide(_retained: float, _obj: KinematicBody2D) -> void:
+	var merge_speed: float = ((1 / speed)) * (1*time) # for improvement
 	yield(get_tree().create_timer(merge_speed), "timeout")
 	queue_free()
 
@@ -73,12 +82,13 @@ func _on_Area2D_area_entered(area) -> void:
 	if mass >= obj.mass:
 		var _loss = engine.generate_random_value(60, 99)
 		var KE_before = (engine.get_kinetic_energy(mass, momentum) + engine.get_kinetic_energy(obj.mass, obj.momentum)) * math.KEx # KE_before = KE1 + KE2
-		var KE_after = (0.5 * (mass + obj.mass) * pow(engine.to_speed(velocity-final), 2)) * math.KEx # KE_final = 1/2 * (m1 + m2) * Δ→v
+		var KE_after = (0.5 * (mass + obj.mass) * pow(math.pythagorean_theorem(velocity.x - final.x, velocity.y - final.y), 2)) * math.KEx # KE_final = 1/2 * (m1 + m2) * Δ→v
 		var KE_loss = 100 - ((KE_after / KE_before) * 100) # get the loss percentage
 		obj.collide(KE_loss, obj)
-		print("============")
-		print(get_name())
-		print(KE_loss)
+		# print("============")
+		# print(get_name())
+		# print(momentum)
+		# print(KE_before - KE_after)
 
 	velocity -=  final
 		
--- Scripts/FrameWorks/Global.gd
+++ Scripts/FrameWorks/Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const UNIVERSE_TIME: float = 0.1 # 1X10 day/s
+const UNIVERSE_TIME: float = 1.0 # 1X10 day/s
 
 var camera_zoom: float = 1.0
 var show_trail: bool = true
